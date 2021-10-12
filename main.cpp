33b35aabab9c09792105ca856d3f2c648171b62e
David G. Wonnacott <davew@cs.haverford.edu>
2021-10-12 15:50:01 -0400

    Basically empty

---
+++ main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by davew on 10/12/21.
+//
+
+#include <iostream>  // access to I.O. Streams library, for std::cout and std::endl
+
+int main() {
+    std::cout << "Hello, world" << std::endl;
+}
