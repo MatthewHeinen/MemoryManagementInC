fe48a74f7668e8e758c0ace55aa46866ad525dbc
mheinen <mheinen>
2022-10-05 15:27:12 -0400

    Crated files for double int

---
+++ doubleInt.c
@@ -0,0 +1,5 @@
+//
+// Created by mheinen on 10/5/22.
+//
+
+#include "doubleInt.h"
---
+++ doubleInt.h
@@ -0,0 +1,8 @@
+//
+// Created by mheinen on 10/5/22.
+//
+
+#ifndef BIGINT_DOUBLEINT_H
+#define BIGINT_DOUBLEINT_H
+
+#endif //BIGINT_DOUBLEINT_H
