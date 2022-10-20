0867a7a3f0901b745c06345401f8df9da5984626
mheinen <mheinen>
2022-10-20 17:09:53 -0400

    Need to restart

--- doubleInt.c
+++ doubleInt.c
@@ -6,33 +6,4 @@
 #include "math.h"
 #include <stdio.h>
 
-void decimalToBinary(unsigned num) {
 
-    // Stores binary representation of number.
-    unsigned binaryNum[32]; // Assuming 32 bit integer.
-    unsigned i=0;
-
-    for ( ;num > 0; ){
-        binaryNum[i++] = num % 2;
-        num /= 2;
-    }
-}
-
-void binaryToDecimal(unsigned num)
-{
-    int dec = 0, i = 0, rem;
-
-    while (num!=0) {
-        rem = num % 10;
-        num /= 10;
-        dec += rem * pow(2, i);
-        i++;
-    }
-}
-
-//Make array for both numbers
-//split array in half to mimic what we did in lab
-//Add function that will add array together and allow carry's
-//Ensure carry goes from beginning part of array to end part of the array
-//Get number out of array
-//Throw error if there is an overflow. deal with in bigInt
--- doubleInt.h
+++ doubleInt.h
@@ -5,6 +5,14 @@
 #ifndef BIGINT_DOUBLEINT_H
 #define BIGINT_DOUBLEINT_H
 
-void decimalToBinary(unsigned num);
+#if defined __cplusplus  /* read this "extern C" line _only_ when we're using C++ */
+extern "C" {
+#endif
+
+
+
+#if defined __cplusplus  /* read this "extern C" line _only_ when we're using C++ */
+}
+#endif
 
 #endif //BIGINT_DOUBLEINT_H
