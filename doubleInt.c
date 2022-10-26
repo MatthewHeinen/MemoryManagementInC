89bbee86b7ca3889880d2a5efbb3da11a2b1fbb7
mheinen <mheinen>
2022-10-26 19:46:04 -0400

    Uploaded what i had so far

--- doubleInt.c
+++ doubleInt.c
@@ -2,8 +2,83 @@
 // Created by mheinen on 10/5/22.
 //
 
+
 #include "doubleInt.h"
-#include "math.h"
-#include <stdio.h>
+#include "safe_int_just_c.h"
+#include <math.h>
+#include <malloc.h>
+#include <string.h>
+#include <stdlib.h>
 
+double_int make_double_int(unsigned int i)
+{
+    double_int result;
+    result.mostSignificant = 0;
+    result.leastSignificant = i;
+    return result;
+}
 
+int make_int_from_double_int(double_int i)
+{
+    if (i.mostSignificant*INT_MAX > INT_MAX - i.leastSignificant) {
+        fprintf(stderr, "Oops, failed due to overflow!");
+        exit(88);
+    } else {
+        return i.mostSignificant * INT_MAX + i.leastSignificant; //TODO: overflow warning. Ask question
+    }
+}
+
+double_int add_double_int(double_int i, double_int j)
+{
+    double_int result;
+    if (i.leastSignificant > INT_MAX - j.leastSignificant || j.leastSignificant > INT_MAX - i.leastSignificant) {
+        if (j.leastSignificant > INT_MAX - i.leastSignificant) {
+            int i_ms = 0;
+            int i_ls = i.leastSignificant;
+            int j_ms = 1;
+            int j_ls = j.leastSignificant - INT_MAX;
+            result.mostSignificant = i_ms + j_ms;
+            result.leastSignificant = i_ls + j_ls;
+            if (i_ls + j_ls == INT_MAX) {
+                result.leastSignificant = i_ls + j_ls - 1;
+            }
+            return  result;
+        } else if (i.leastSignificant > INT_MAX - j.leastSignificant) {
+            int i_ms = 1;
+            int i_ls = i.leastSignificant - INT_MAX;
+            int j_ms = 0;
+            int j_ls = j.leastSignificant;
+            result.mostSignificant = i_ms + j_ms;
+            result.leastSignificant = i_ls + j_ls;
+            if (i_ls + j_ls == INT_MAX) {
+                result.leastSignificant = i_ls + j_ls - 1;
+            }
+            return  result;
+        }
+    } else {
+        int res = i.leastSignificant + j.leastSignificant;
+        double_int no_overflow_result = make_double_int(res);
+        return no_overflow_result;
+    }
+    return result;
+}
+
+double_int double_int_add_to(double_int * i, double_int j) {
+    if (i->leastSignificant > INT_MAX - j.leastSignificant || j.leastSignificant > INT_MAX - i->leastSignificant) {
+        fprintf(stderr, "Oops, failed to add. Overflow!");
+        exit(88);
+    } else if ((i->mostSignificant  > 0 && j.mostSignificant > 0 ) && (i->leastSignificant > 0 || j.leastSignificant > 0)) {
+        fprintf(stderr, "Oops, failed to add. Overflow!");
+        exit(88);
+    } else if ((i->mostSignificant > 0 || j.mostSignificant > 0) && (i->leastSignificant > INT_MAX - j.leastSignificant)) {
+        fprintf(stderr, "Oops, failed to add. Overflow!");
+        exit(88);
+    } else {
+        *i =  add_double_int(*i, j);
+        return *i;
+    }
+}
+
+void double_int_demo() {
+
+}
--- doubleInt.h
+++ doubleInt.h
@@ -2,17 +2,27 @@
 // Created by mheinen on 10/5/22.
 //
 
-#ifndef BIGINT_DOUBLEINT_H
-#define BIGINT_DOUBLEINT_H
+#ifndef BIGINT_DOUBLE_INT_H
+#define BIGINT_DOUBLE_INT_H
+
 
 #if defined __cplusplus  /* read this "extern C" line _only_ when we're using C++ */
 extern "C" {
 #endif
+typedef int double_int_base_type;
 
+typedef struct {
+    double_int_base_type mostSignificant;
+    double_int_base_type leastSignificant;
+} double_int;
 
-
+double_int make_double_int(unsigned int i);
+double_int double_int_add_to(double_int * i, double_int j);
+int make_int_from_double_int(double_int i);
+double_int add_double_int(double_int i, double_int j);
+void double_int_demo();
 #if defined __cplusplus  /* read this "extern C" line _only_ when we're using C++ */
 }
 #endif
 
-#endif //BIGINT_DOUBLEINT_H
+#endif //BIGINT_DOUBLE_INT_H
